4205bfc02282240323bf25bebd62f89976e3de6b
Romulo Goncalves <romulogoncalves@posteo.de>
2020-04-07 17:00:25 +0200

    Rename to tutorial

---
+++ tutorial/scripts/initializedb.py
@@ -0,0 +1,14 @@
+# initializedb.py
+from sqlalchemy import engine_from_config
+from tutorial import SQLALCHEMY_URL
+from tutorial.models import Base
+from sqlalchemy import create_engine
+import os
+
+
+def main():
+    """Tear down existing tables and create new ones."""
+    engine = create_engine(os.environ.get('DATABASE_URL'))
+    if bool(os.environ.get('DEBUG', '')):
+        Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine, Base.metadata.tables.values(), checkfirst=True)
