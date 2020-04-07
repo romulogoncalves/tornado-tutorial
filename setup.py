4ac0dad099155fcd7509e9158da035a0dc88d56d
Romulo Goncalves <romulogoncalves@posteo.de>
2020-04-07 17:02:19 +0200

    Dos2unix and add new dependencies

--- setup.py
+++ setup.py
@@ -6,14 +6,16 @@ requires = [
     'tornado-sqlalchemy',
     'sqlalchemy',
     'psycopg2',
-    'passlib'
+    'passlib',
+    'aiopg',
+    'bcrypt'
 ]
 
 setup(
     name='tornado_todo',
     version='0.0',
-    description='A To-Do List built with Tornado',
-    author='<Your name>',
+    description='A tutorial with Tornado',
+    author='Romulo Goncalves',
     author_email='<Your email>',
     keywords='web tornado',
     packages=find_packages(),
@@ -21,8 +23,8 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'serve_app = todo:main',
-            'initdb = todo.scripts.initializedb:main',
+            'serve_app = tutorial:main',
+            'initdb = tutorial.scripts.initializedb:main',
         ],
     },
 )
